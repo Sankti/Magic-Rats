d3b61f97038942ff83471c490a7a418ab59f6a63
Adam <57396704+Sankti@users.noreply.github.com>
2020-01-03 15:38:46 +0100

    Update main.py
    Updated the file: erased unused functionality of a text box.

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from tkinter import *
 
 def show_info():
-    message_txt.delete(0.0, 'end')
     selected_rat = rat_entry.get()
 
     for label in magic_rats.grid_slaves():
-        if int(label.grid_info()["row"]) == 6:
+        if int(label.grid_info()["row"]) == 5:
             label.grid_forget()
 
     if selected_rat == "Carrion Rats":
-        carrion_rats_photo_label.grid(row=6, columnspan=3)
+        carrion_rats_photo_label.grid(row=5, columnspan=3)
     elif selected_rat == "Pack Rat":
-        pack_rat_photo_label.grid(row=6, columnspan=3)
+        pack_rat_photo_label.grid(row=5, columnspan=3)
     else:
-        message_txt.insert(0.0, "You are not authorized!")
-        restricted_photo_label.grid(row=6, columnspan=3)
+        restricted_photo_label.grid(row=5, columnspan=3)
 
 magic_rats = Tk()
 
@@ -31,7 +29,6 @@ rat_dropdown = OptionMenu(magic_rats, rat_entry, "Carrion Rats", "Pack Rat")
 summon_button = Button(magic_rats, text="Summon!", font=("arial", 12), command=lambda: show_info())
 
 magic_rats.grid_rowconfigure(3, minsize=20)
-message_txt = Text(magic_rats, width=16, height=1, wrap=WORD, fg="red", font=("arial", 20, "bold"))
 
 carrion_rats_img = PhotoImage(file="carrion_rats.gif")
 carrion_rats_photo_label = Label(magic_rats, image=carrion_rats_img)
@@ -44,6 +41,5 @@ question.grid(row=1, columnspan=1)
 rat_dropdown.grid(row=3, sticky=E)
 summon_button.grid(row=4, sticky=E)
 
-message_txt.grid(row=5, columnspan=3, sticky=N, rowspan=1)
 
 magic_rats.mainloop()
